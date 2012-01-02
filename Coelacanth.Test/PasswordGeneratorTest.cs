45ff2fd8f33b1bdf598ca78f06f784b37f2e40f6
robert.impey <robert.impey@b12e0cf6-aa41-0410-a8c4-ddcd1f90e0db>
2012-01-02 22:15:46 +0000

    Test that the passwords don't contain characters that have not been asked for.

--- Coelacanth.Test/PasswordGeneratorTest.cs
+++ Coelacanth.Test/PasswordGeneratorTest.cs
@@ -1,19 +1,18 @@
 ﻿using Coelacanth.Lib;
 using NUnit.Framework;
+using System.Text.RegularExpressions;
 
 namespace Coelacanth.Test
 {
     [TestFixture]
     public class PasswordGeneratorTest
     {
-        [Test]
-        public void GeneratePassword()
-        {
-            var passwordGenerator = new PasswordGenerator();
+        #region Tests
 
-            passwordGenerator.IncludeUppercase = true;
-            passwordGenerator.IncludeLowercase = true;
-            passwordGenerator.IncludeDigits = true;
+        [Test]
+        public void PwGenGeneratesPwOfCorrectLength()
+        {
+            var passwordGenerator = DefaultPasswordGenerator();
 
             var passwordLength = 20;
             passwordGenerator.PasswordLength = passwordLength;
@@ -22,5 +21,59 @@ namespace Coelacanth.Test
 
             Assert.AreEqual(passwordLength, password.Length);
         }
+
+        [Test]
+        public void PwGenWoUcDoesNotGeneratePwWithUc()
+        {
+            var passwordGenerator = DefaultPasswordGenerator();
+
+            passwordGenerator.IncludeUppercase = false;
+
+            var password = passwordGenerator.NewPassword();
+
+            Assert.IsFalse(Regex.IsMatch(password, "[A-Z]+"));
+        }
+
+        [Test]
+        public void PwGenWoLcDoesNotGeneratePwWithLc()
+        {
+            var passwordGenerator = DefaultPasswordGenerator();
+
+            passwordGenerator.IncludeLowercase = false;
+
+            var password = passwordGenerator.NewPassword();
+
+            Assert.IsFalse(Regex.IsMatch(password, "[a-z]+"));
+        }
+
+        [Test]
+        public void PwGenWoDigitsDoesNotGeneratePwWithDigits()
+        {
+            var passwordGenerator = DefaultPasswordGenerator();
+
+            passwordGenerator.IncludeDigits = false;
+
+            var password = passwordGenerator.NewPassword();
+
+            Assert.IsFalse(Regex.IsMatch(password, "[0-9]+"));
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static PasswordGenerator DefaultPasswordGenerator()
+        {
+            var passwordGenerator = new PasswordGenerator();
+
+            passwordGenerator.IncludeUppercase = true;
+            passwordGenerator.IncludeLowercase = true;
+            passwordGenerator.IncludeDigits = true;
+            passwordGenerator.PasswordLength = 20;
+
+            return passwordGenerator;
+        }
+
+        #endregion
     }
 }
